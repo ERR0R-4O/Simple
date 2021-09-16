92d221e0f51460ec93bc8e55925d23ad477a2a02
ERR0R-4O <90842326+ERR0R-4O@users.noreply.github.com>
2021-09-16 09:54:15 -0400

    ArithmeticCalculator.java
    Simple Calculator Program, will update over time.

---
+++ ArithmeticCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner
+
+class Main {
+  public static void main(String[] args){
+    Scanner stinky = new Scanner(System.in);
+    System.out.println("Enter first number for math problem");
+    int firstNum = stinky.nextInt();
+    Scanner poopoo = new Scanner(System.in);
+    System.out.println("What type of problem: Addition, Subtraction, Multiplication, or Division? (Type Exactly!)")
+    String typeOfProblem = poopoo.nextString;
+    Scanner haha = new Scanner(System.in);
+    System.out.println("Enter second number for math problem");
+    int secondNum = haha.nextInt();
+    
+    if(typeOfProblem == "Addition"){
+      System.out.println(firstNum + secondNum);
+     }elseif(typeOfProblem == "Subtraction"){
+      System.out.println(firstNum - secondNum);
+     }elseif(typeOfProblem == "Multiplication"){
+      System.out.println(firstNum * secondNum);
+     }elseif(typeOfProblem == "Division"){
+      System.out.println(firstNum / secondNum);
+     } else{
+      System.out.println("That is not an acceptable problem type, restart the program and retype the problem type");
+      
+    
+    
+   
