30f2a51789e04297b238e7a6923256a6671b4d1a
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-09-17 19:01:27 -0400

    Update ArithmeticCalculator.py

--- ArithmeticCalculator.py
+++ ArithmeticCalculator.py
@@ -1,22 +1,34 @@
 def calc():
-  numOne = int(input("What is the first number of your problem?"))
-  numTwo = int(input("What is the second number of your problem?"))
-  numThree = input("What type of Math Problem is it, Addition, Subtraction, Multiplication, Division, Remainder, or Exponents? Type exactly.")
+  numOne = int(input("What is the first number of your problem?(x)"))
+  numTwo = int(input("What is the second number of your problem?(y)"))
+  numThree = input("Please type code for any following function: AD for Addition, SUB for Subtraction, MUL for Multiplication, DIV for Division, LOG for Base 10 LOG, EX for Exponents, MOD for Modulo")
 
-  if numThree == 'Addition':
+  if numThree == 'AD':
     print(numOne + numTwo)
-  elif numThree == 'Subtraction':
+  elif numThree == 'SUB':
     print(numOne - numTwo)
-  elif numThree == 'Multiplication':
+  elif numThree == 'MUL':
    print(numOne * numTwo)
-  elif numThree == 'Division':
+  elif numThree == 'DIV':
     print(numOne / numThree)
-  elif numThree == 'Remainder':
-    print(numOne % numTwo)
-  elif numThree == 'Exponents':
+  elif numThree == 'LOG':
+    logb10=math.log10(x)
+    print(logb10)
+  elif numThree == 'EX':
     print(numOne ** numTwo)
   else:
-    print("Not acceptable format. Restart program and run again.")
-
+    print("Please enter on of the above functions. ")
 while True:
   calc()
+  
+  repeat()
+  
+  def repeat():
+    repeat_program=input('''Do you want to calculate again, Yes or No?''')
+    
+    if calc_again.upper() == 'Yes':
+        calculate()
+    elif calc_again.upper() == 'No':
+        print('Thanks for using my calculator.')
+    else:
+        again()
