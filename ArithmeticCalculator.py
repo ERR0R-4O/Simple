37a0942e8a431f1543df368f451278a62105794b
ERR0R-4O <90842326+ERR0R-4O@users.noreply.github.com>
2021-09-21 09:16:48 -0400

    Update ArithmeticCalculator.py
    Added Factorials

--- ArithmeticCalculator.py
+++ ArithmeticCalculator.py
@@ -1,7 +1,9 @@
+import math
+
 def calc():
   numOne = int(input("What is the first number of your problem?"))
   numTwo = int(input("What is the second number of your problem?"))
-  numThree = input("What type of Math Problem is it, Addition, Subtraction, Multiplication, Division, Remainder, or Exponents? Type exactly.")
+  numThree = input("What type of Math Problem is it, Addition, Subtraction, Multiplication, Division, Remainder, Exponents, or Factorials? Type exactly.")
 
   if numThree == 'Addition':
     print(numOne + numTwo)
@@ -15,8 +17,16 @@ def calc():
     print(numOne % numTwo)
   elif numThree == 'Exponents':
     print(numOne ** numTwo)
+  elif numThree == 'Factorials':
+    thing = input("Which number do you want to use, the first or second? Type 1 for the first one or 2 for the second.")
+    if thing == '1':
+      print(math.factorial(numOne))
+    elif thing == '2':
+      print(math.factorial(numTwo))
+    else:
+      print("Not acceptable format. Wait for the program to restart and run again.")
   else:
-    print("Not acceptable format. Restart program and run again.")
+    print("Not acceptable format. Wait for program restart and try again")
 
 while True:
   calc()
